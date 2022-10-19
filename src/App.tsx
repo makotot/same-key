e0dd5fda006a9ba9c53bd92b32ac8e5dc974846e
makotot <makoto.ttn@gmail.com>
2022-10-19 21:50:22 +0900

    orphan

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const items = [
+  [
+    {id: 0, name: "A------------------------------"},// https://github.com/facebook/react/issues/24871 lose tracking
+    {id: 0, name: "A"},
+    {id: 1, name: "B"},
+    {id: 2, name: "C"}
+  ],
+  [
+    {id: 3, name: "D"},
+  ],
+]
+
 function App() {
+  const [current, updateIndex] = useState<0 | 1>(0)
+
+  useEffect(() => {
+    return () => {
+      console.log(current)
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => updateIndex(current === 0 ? 1 : 0)}>Switch</button>
+      <ul>
+        {
+          items[current].map((item) => {
+            return (
+              <li key={item.id}>
+                {item.id} : {item.name}
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
